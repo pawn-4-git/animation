577c45d89606acc90d5e01e8cf93d44be841d4ae
pawn4git <@192.168.1.100>
2018-01-15 00:12:50 +0900

    オープニングおよびエンディングの設定可能にする

--- animation/src/infomation/EpisodeInfo.java
+++ animation/src/infomation/EpisodeInfo.java
@@ -14,14 +14,76 @@ import java.util.List;
  * @author Pawn
  */
 public class EpisodeInfo {
-    List<BaseEvent> eventList=new ArrayList<>();
-    int episodeNo=-1;
+    private List<BaseEvent> eventList=new ArrayList<>();
+    private int episodeNo=-1;
+    
+    private Music oping;
+    
+    private Music ending;
     
     public EpisodeInfo(int episodeNo){
         this.episodeNo=episodeNo;
     }
     
     public void add(BaseEvent baseEvent){
-        eventList.add(baseEvent);
+        getEventList().add(baseEvent);
     }
+
+    /**
+     * @return the eventList
+     */
+    public List<BaseEvent> getEventList() {
+        return eventList;
+    }
+
+    /**
+     * @param eventList the eventList to set
+     */
+    public void setEventList(List<BaseEvent> eventList) {
+        this.eventList = eventList;
+    }
+
+    /**
+     * @return the episodeNo
+     */
+    public int getEpisodeNo() {
+        return episodeNo;
+    }
+
+    /**
+     * @param episodeNo the episodeNo to set
+     */
+    public void setEpisodeNo(int episodeNo) {
+        this.episodeNo = episodeNo;
+    }
+
+    /**
+     * @return the oping
+     */
+    public Music getOping() {
+        return oping;
+    }
+
+    /**
+     * @param oping the oping to set
+     */
+    public void setOping(Music oping) {
+        this.oping = oping;
+    }
+
+    /**
+     * @return the ending
+     */
+    public Music getEnding() {
+        return ending;
+    }
+
+    /**
+     * @param ending the ending to set
+     */
+    public void setEnding(Music ending) {
+        this.ending = ending;
+    }
+    
+    
 }
